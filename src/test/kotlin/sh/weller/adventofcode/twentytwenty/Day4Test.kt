f46c31a795dab2d642c4e1955cb92b9d09b3899b
Guruth <git@weller.sh>
2020-12-04 07:19:45 +0100

    Ugly as hell Day 4 solution

--- src/test/kotlin/sh/weller/adventofcode/twentytwenty/Day4Test.kt
+++ src/test/kotlin/sh/weller/adventofcode/twentytwenty/Day4Test.kt
@@ -1,7 +1,9 @@
 package sh.weller.adventofcode.twentytwenty
 
 import sh.weller.adventofcode.util.fileToList
+import sh.weller.adventofcode.util.printResult
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Day4Test {
     val testData = fileToList<String>("2020/Day4TestData.txt")
@@ -9,17 +11,47 @@ class Day4Test {
 
     @Test
     fun partOneTest() {
+        val result = testData.validatePassports(expectedFieldKeys)
+        assertEquals(2, result)
     }
 
     @Test
     fun partOneReal() {
+        val result = realData.validatePassports(expectedFieldKeys)
+        printResult(4, 1, result)
     }
 
     @Test
     fun partTwoTest() {
+        val result = testData.validatePassportsPartTwo(expectedFieldKeys)
+        assertEquals(0, result)
     }
 
     @Test
     fun partTwoReal() {
+        val result = realData.validatePassportsPartTwo(expectedFieldKeys)
+        printResult(4, 2, result)
     }
 }
+
+val expectedFields = listOf(
+    "byr (Birth Year)",
+    "iyr (Issue Year)",
+    "eyr (Expiration Year)",
+    "hgt (Height)",
+    "hcl (Hair Color)",
+    "ecl (Eye Color)",
+    "pid (Passport ID)",
+    "cid (Country ID)"
+)
+
+val expectedFieldKeys = listOf(
+    "byr",
+    "iyr",
+    "eyr",
+    "hgt",
+    "hcl",
+    "ecl",
+    "pid",
+//    "cid"
+)
