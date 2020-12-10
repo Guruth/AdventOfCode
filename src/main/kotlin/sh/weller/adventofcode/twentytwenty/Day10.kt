04740438b6455dfde46fa4c56ad1443798c932aa
Guruth <git@weller.sh>
2020-12-10 20:18:27 +0100

    Day 10 Cleanup

--- src/main/kotlin/sh/weller/adventofcode/twentytwenty/Day10.kt
+++ src/main/kotlin/sh/weller/adventofcode/twentytwenty/Day10.kt
@@ -1,11 +1,10 @@
 package sh.weller.adventofcode.twentytwenty
 
 
-fun List<Int>.findPossibleVariations_bruteforce(): Int {
+fun List<Int>.findPossibleVariationsBruteForce(): Int {
     val sorted = (listOf(0) + this.sorted() + listOf(this.maxOf { it } + 3))
     return sorted.rec().size
 }
-
 
 private fun List<Int>.rec(): List<Int> {
     if (this.size == 1) {
@@ -23,27 +22,26 @@ fun List<Int>.findPossibleVariations(): Long {
 
     sorted.forEachIndexed { index, i ->
         var counter = 0L
-        val tmpList = sorted.take(index).takeLast(3)
-        tmpList.forEachIndexed { innerIndex, j ->
+        sorted.take(index).takeLast(3).forEachIndexed { innerIndex, j ->
             if (i - 3 <= j) {
-                val atPoint = depthList.getOrNull(index - innerIndex) ?: 0L
+                val atPoint = depthList.getOrElse(index - innerIndex) { 0L }
                 counter += atPoint
             }
         }
         depthList.add(maxOf(counter, depthList[index]))
+        println(sorted.take(index).takeLast(3))
     }
     return depthList.last()
 }
 
 
-fun List<Int>.countJoltDiffereneces(startJoltage: Int = 0): Long {
+fun List<Int>.countJoltDifferences(startJoltage: Int = 0): Long {
     var currentJoltage = startJoltage
     var oneDifference: Long = 0
     var threeDifference: Long = 1
 
     this.sorted().forEach { value ->
         val difference = value - currentJoltage
-        println("Diff: $difference")
         if (difference >= 1 || difference <= 3) {
             when (difference) {
                 1 -> {
@@ -52,19 +50,9 @@ fun List<Int>.countJoltDiffereneces(startJoltage: Int = 0): Long {
                 3 -> {
                     threeDifference++
                 }
-                else -> {
-                    println("Does not fit the difference: $value")
-                }
             }
             currentJoltage += difference
-        } else {
-            println("Can't use this adapter $value")
         }
     }
-
-
-    println("Cur: $currentJoltage one: $oneDifference three: $threeDifference")
-
     return oneDifference * threeDifference
-
 }
