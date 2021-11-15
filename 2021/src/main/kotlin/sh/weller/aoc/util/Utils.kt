444f9b5e152183459dbaa19c250dc1eea3e4bc84
Guruth <1669916+Guruth@users.noreply.github.com>
2021-11-15 22:16:24 +0100

    Added project for 2021

---
+++ 2021/src/main/kotlin/sh/weller/aoc/util/Utils.kt
@@ -0,0 +1,24 @@
+package sh.weller.aoc.util
+
+fun List<Number>.product(): Long =
+    this.map { it.toLong() }
+        .reduce { i, j -> i * j }
+
+fun printResult(day: Int, part: Int, result: Any) {
+    println("Result Day $day-$part: $result")
+}
+
+fun <T> List<T>.copy(): List<T> =
+    this.toList()
+
+@JvmName("copyListDepth2")
+fun <T> List<List<T>>.copy(): List<List<T>> =
+    this.map { it.copy() }
+
+@JvmName("copyListDepth3")
+fun <T> List<List<List<T>>>.copy(): List<List<List<T>>> =
+    this.map { it.copy() }
+
+@JvmName("copyListDepth4")
+fun <T> List<List<List<List<T>>>>.copy(): List<List<List<List<T>>>> =
+    this.map { it.copy() }
---
+++ 2021/src/test/kotlin/sh/weller/aoc/util/FileSupportTest.kt
@@ -0,0 +1,61 @@
+package sh.weller.aoc.util
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class FileSupportTest {
+    @Test
+    fun fileToIntList() {
+        val expectedData = listOf(
+            123,
+            1123,
+            12313,
+            123
+        )
+
+        val result = fileToList<Int>("util/intList.txt")
+        assertEquals(expectedData, result)
+    }
+
+    @Test
+    fun fileToDoubleList() {
+        val expectedData = listOf(
+            123.0,
+            1123.0,
+            12313.34,
+            123.0
+        )
+
+        val result = fileToList<Double>("util/doubleList.txt")
+        assertEquals(expectedData, result)
+    }
+
+
+    @Test
+    fun fileToInvalidList() {
+        val expectedData = listOf(
+            123,
+        )
+
+        val result = fileToList<Int>("util/invalidList.txt")
+        assertEquals(expectedData, result)
+    }
+
+    @Test
+    fun notFoundList() {
+        val expectedData = emptyList<Any>()
+        val result = fileToList<Int>("util/foo.txt")
+        assertEquals(expectedData, result)
+    }
+
+    @Test
+    fun fileTo2dListTest() {
+        val expectedData = listOf(
+            listOf('*', '#', '*'),
+            listOf('*', '*', '*'),
+            listOf('*', '*', '#')
+        )
+        val result = fileTo2DList("util/2dList.txt")
+        assertEquals(expectedData, result)
+    }
+}
