b1893af6a75576977be9f83ffd6fc176a47eb893
Guruth <1669916+Guruth@users.noreply.github.com>
2021-12-01 08:36:13 +0100

    Optimized Day01 Solution

--- 2021/src/main/kotlin/sh/weller/aoc/Day01.kt
+++ 2021/src/main/kotlin/sh/weller/aoc/Day01.kt
@@ -3,18 +3,18 @@ package sh.weller.aoc
 object Day01 {
 
     fun first(numbers: List<Int>): Int {
-        return numbers.windowed(2)
-            .map { (first, second) ->
+        return numbers
+            .windowed(2) { (first, second) ->
                 second > first
-            }.count { it }
+            }
+            .count { it }
 
     }
 
     fun second(numbers: List<Int>): Int {
-       return numbers.windowed(3)
-            .map { it.sum() }
-            .windowed(2)
-            .map { (first, second) ->
+        return numbers
+            .windowed(3) { it.sum() }
+            .windowed(2) { (first, second) ->
                 second > first
             }
             .count { it }
