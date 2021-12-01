17c52cfe9163d7d90bb7b0c02044bb82b2d1cc89
Guruth <1669916+Guruth@users.noreply.github.com>
2021-12-01 08:29:27 +0100

    Day01 Solution

--- 2021/src/test/kotlin/sh/weller/aoc/Day01Test.kt
+++ 2021/src/test/kotlin/sh/weller/aoc/Day01Test.kt
@@ -1,35 +1,37 @@
 package sh.weller.aoc
 
 import sh.weller.aoc.util.fileToList
+import sh.weller.aoc.util.printResult
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Day01Test {
     private val day = 1
-    private val testData = fileToList<String>("Day${day}TestData.txt")
-    private val realData = fileToList<String>("Day${day}Data.txt")
+    private val testData = fileToList<Int>("Day${day}TestData.txt")
+    private val realData = fileToList<Int>("Day${day}Data.txt")
 
     @Test
     fun partOneTest() {
-//        val result = testData.
-//        assertEquals(0, result)
+        val result = Day01.first(testData)
+        assertEquals(7, result)
     }
 
     @Test
     fun partOneReal() {
-//        val result = realData.
-//        printResult(day, 1, result)
+        val result = Day01.first(realData)
+        printResult(day, 1, result)
     }
 
     @Test
     fun partTwoTest() {
-//        val result = testData.
-//        assertEquals(0, result)
+        val result = Day01.second(testData)
+        assertEquals(5, result)
     }
 
     @Test
     fun partTwoReal() {
-//        val result = realData.
-//        printResult(day, 2, result)
+        val result = Day01.second(realData)
+        printResult(day, 2, result)
     }
 }
 
