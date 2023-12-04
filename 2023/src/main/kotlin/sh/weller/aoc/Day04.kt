1e530ec801c6cd5cbeff8e34a746438c41a901ee
Weller, Kai <Kai.Weller@vector.com>
2023-12-04 11:00:26 +0100

    Added Day04

--- 2023/src/main/kotlin/sh/weller/aoc/Day04.kt
+++ 2023/src/main/kotlin/sh/weller/aoc/Day04.kt
@@ -1,7 +1,5 @@
 package sh.weller.aoc
 
-import sh.weller.aoc.SomeDay
-
 object Day04 : SomeDay<Pair<List<Int>, List<Int>>, Int> {
     override fun partOne(input: List<Pair<List<Int>, List<Int>>>): Int =
         input
@@ -22,8 +20,28 @@ object Day04 : SomeDay<Pair<List<Int>, List<Int>>, Int> {
             .sum()
 
     override fun partTwo(input: List<Pair<List<Int>, List<Int>>>): Int {
-        TODO("Not yet implemented")
+        val cards = input
+            .map { (winningNumbers, havingNumbers) -> Triple(1, winningNumbers, havingNumbers) }
+            .toMutableList()
+
+        for (i in 0..<cards.size) {
+            val (numberOfCopies, winningNumbers, havingNumbers) = cards[i]
+            val numberOfMatches = getNumberOfMatches(winningNumbers, havingNumbers)
+
+
+            for (j in i + 1..i + numberOfMatches) {
+                val card = cards
+                    .getOrNull(j) ?: break
+
+                cards[j] = card.copy(first = card.first + numberOfCopies)
+            }
+        }
+        return cards
+            .sumOf { it.first }
     }
 
-
+    private fun getNumberOfMatches(winningNumbers: List<Int>, havingNumbers: List<Int>): Int =
+        havingNumbers
+            .filter { it in winningNumbers }
+            .size
 }
