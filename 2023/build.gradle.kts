eb7c5c12285c143f3297fc963d2836a0eb895834
Weller, Kai <Kai.Weller@vector.com>
2023-12-05 15:47:59 +0100

    Implemented Day5

--- 2023/build.gradle.kts
+++ 2023/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.20"
     idea
 }
 
@@ -18,7 +18,6 @@ dependencies {
 //    implementation("org.magicwerk:brownies-collections:0.9.15")
 //    implementation("org.jetbrains.kotlinx:multik-api:0.1.1")
 //    implementation("org.jetbrains.kotlinx:multik-default:0.1.1")
-
 
     testImplementation(kotlin("test"))
 }
