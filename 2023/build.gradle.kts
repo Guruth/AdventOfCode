e5505f796420c844a7ac939399692c5351999b43
Guruth <1669916+Guruth@users.noreply.github.com>
2023-12-09 23:11:40 +0100

    Added  Day09

--- 2023/build.gradle.kts
+++ 2023/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "1.9.21"
     idea
 }
 
@@ -27,5 +27,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "21"
 }
