6157077d5a7f9c95a4cae38e62d4f7cb43f7ad75
Guruth <1669916+Guruth@users.noreply.github.com>
2024-12-12 20:09:19 +0100

    Added 2015 Day1 Solution

---
+++ 2015/src/util.rs
@@ -0,0 +1,12 @@
+use std::fs::read_to_string;
+
+pub fn load_file(file_name: &str) -> String {
+    println!("Loading file: {}", file_name);
+    return read_to_string(format!("./data/{}.txt", file_name))
+        .expect("Could not read file!");
+}
+
+
+pub fn print_result(day: i8, part: i8, result: i64) {
+    println!("Result of Day {} Part {} is {}", day, part, result);
+}
