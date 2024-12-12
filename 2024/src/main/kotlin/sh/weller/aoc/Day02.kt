08aad8ab1c87c17e064c99bb1f86da52e84bc3bf
Guruth <1669916+Guruth@users.noreply.github.com>
2024-12-12 20:10:02 +0100

    Prettier Day02 Solution with part 2

--- 2024/src/main/kotlin/sh/weller/aoc/Day02.kt
+++ 2024/src/main/kotlin/sh/weller/aoc/Day02.kt
@@ -1,6 +1,5 @@
 package sh.weller.aoc
 
-import sun.misc.Unsafe
 import kotlin.math.abs
 
 object Day02 : SomeDay<Int> {
@@ -14,35 +13,34 @@ object Day02 : SomeDay<Int> {
     override fun partOne(input: List<String>): Int {
         val reports = input.parseData()
 
-        return reports.count {
-            it.isSafe()
-        }
+        return reports
+            .count {
+                it.isSafe()
+            }
     }
 
     private fun List<Int>.isSafe(): Boolean {
-        return windowed(2)
-            .map { (first, second) ->
-                if (abs(first - second) !in 1..3) {
-                    return@map Direction.Unsafe
-                }
-                if (first > second) {
-                    return@map Direction.Dec
-                }
-                if (second > first()) {
-                    return@map Direction.Inc
-                }
-                return@map null
-            }
-            .groupingBy { it }.eachCount().size == 1
+        val distanceIsSafe = windowed(2)
+            .all { (a, b) -> abs(a - b) in 1..3 }
+
+        val sortedMatch = sorted() == this
+        val reverseSortedMatch = sorted().reversed() == this
+
+        return distanceIsSafe && (sortedMatch || reverseSortedMatch)
     }
 
     override fun partTwo(input: List<String>): Int {
+        val reports = input.parseData()
 
-        return -1
+        return reports.count {
+            val reportIsSafe = it.isSafe()
+
+            val anyReportWithOneMissingIsSafe = (0..it.size)
+                .any { index ->
+                    (it.take(index) + it.drop(index + 1)).isSafe()
+                }
+
+            reportIsSafe || anyReportWithOneMissingIsSafe
+        }
     }
-
-}
-
-enum class Direction {
-    Inc, Dec, Unsafe;
 }
