4f210ac6cef8e96eecf1739cf0a2bce0f9119d0c
Guruth <1669916+Guruth@users.noreply.github.com>
2024-12-12 20:09:18 +0100

    Added Template & Day 6 preparation

---
+++ src/test/kotlin/sh/weller/adventofcode/twentytwenty/Day6Test.kt
@@ -0,0 +1,35 @@
+package sh.weller.adventofcode.twentytwenty
+
+import sh.weller.adventofcode.util.fileToList
+import kotlin.test.Test
+
+class Day6Test {
+    private val day = 6
+    private val testData = fileToList<String>("2020/Day${day}TestData.txt")
+    private val realData = fileToList<String>("2020/Day${day}Data.txt")
+
+    @Test
+    fun partOneTest() {
+//        val result = testData.
+//        assertEquals(0, result)
+    }
+
+    @Test
+    fun partOneReal() {
+//        val result = realData.
+//        printResult(6, 1, result)
+    }
+
+    @Test
+    fun partTwoTest() {
+//        val result = testData.
+//        assertEquals(0, result)
+    }
+
+    @Test
+    fun partTwoReal() {
+//        val result = realData.
+//        printResult(6, 2, result)
+    }
+}
+
---
+++ src/test/kotlin/sh/weller/adventofcode/twentytwenty/TestTemplate.kt
@@ -0,0 +1,35 @@
+package sh.weller.adventofcode.twentytwenty
+
+import sh.weller.adventofcode.util.fileToList
+import kotlin.test.Test
+
+class TestTemplate {
+    private val day = 0
+    private val testData = fileToList<String>("2020/Day${day}TestData.txt")
+    private val realData = fileToList<String>("2020/Day${day}Data.txt")
+
+    @Test
+    fun partOneTest() {
+//        val result = testData.
+//        assertEquals(0, result)
+    }
+
+    @Test
+    fun partOneReal() {
+//        val result = realData.
+//        printResult(day, 1, result)
+    }
+
+    @Test
+    fun partTwoTest() {
+//        val result = testData.
+//        assertEquals(0, result)
+    }
+
+    @Test
+    fun partTwoReal() {
+//        val result = realData.
+//        printResult(day, 2, result)
+    }
+}
+
