f75d7ec3ae96826db547e08b88db4e6637651e50
Guruth <1669916+Guruth@users.noreply.github.com>
2024-12-12 20:09:19 +0100

    Day15 Solution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.20"
+    kotlin("jvm") version "1.4.21"
 }
 
 group = "sh.weller"
@@ -15,8 +15,8 @@ dependencies {
     implementation("org.jgrapht:jgrapht-core:1.5.0")
 
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
 tasks.test {
