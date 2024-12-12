bd1c2a6bf4441ed6abfd9747c45d0e3f6ffb95a8
Guruth <1669916+Guruth@users.noreply.github.com>
2024-12-12 20:09:20 +0100

    Added Test for Day01

--- 2021/src/test/kotlin/sh/weller/aoc/Day01Test.kt
+++ 2021/src/test/kotlin/sh/weller/aoc/Day01Test.kt
@@ -1,2 +1,35 @@
 package sh.weller.aoc
 
+import sh.weller.aoc.util.fileToList
+import kotlin.test.Test
+
+class Day01Test {
+    private val day = 0
+    private val testData = fileToList<String>("Day${day}TestData.txt")
+    private val realData = fileToList<String>("Day${day}Data.txt")
+
+    @Test
+    fun partOneTest() {
+//        val result = testData.
+//        assertEquals(0, result)
+    }
+
+    @Test
+    fun partOneReal() {
+//        val result = realData.
+//        printResult(day, 1, result)
+    }
+
+    @Test
+    fun partTwoTest() {
+//        val result = testData.
+//        assertEquals(0, result)
+    }
+
+    @Test
+    fun partTwoReal() {
+//        val result = realData.
+//        printResult(day, 2, result)
+    }
+}
+
