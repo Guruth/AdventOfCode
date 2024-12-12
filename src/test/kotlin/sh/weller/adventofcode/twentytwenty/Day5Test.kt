8909e1468190f0abff8162227259fae7e146ded7
Guruth <1669916+Guruth@users.noreply.github.com>
2024-12-12 20:09:18 +0100

    Day 5 preparation

---
+++ src/test/kotlin/sh/weller/adventofcode/twentytwenty/Day5Test.kt
@@ -0,0 +1,33 @@
+package sh.weller.adventofcode.twentytwenty
+
+import sh.weller.adventofcode.util.fileToList
+import kotlin.test.Test
+
+class Day5Test {
+    val testData = fileToList<String>("2020/Day5TestData.txt")
+    val realData = fileToList<String>("2020/Day5Data.txt")
+
+    @Test
+    fun partOneTest() {
+//        val result =
+//        assertEquals(0, result)
+    }
+
+    @Test
+    fun partOneReal() {
+//        val result =
+//        printResult(5, 1, result)
+    }
+
+    @Test
+    fun partTwoTest() {
+//        val result =
+//        assertEquals(0, result)
+    }
+
+    @Test
+    fun partTwoReal() {
+//        val result =
+//        printResult(5, 2, result)
+    }
+}
