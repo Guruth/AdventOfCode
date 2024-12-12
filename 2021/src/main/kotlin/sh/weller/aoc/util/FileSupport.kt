e7ee5264195682370b6ff7c6609adb02aa0b67a8
Guruth <1669916+Guruth@users.noreply.github.com>
2024-12-12 20:09:20 +0100

    Added project for 2021

---
+++ 2021/src/main/kotlin/sh/weller/aoc/util/FileSupport.kt
@@ -0,0 +1,26 @@
+package sh.weller.aoc.util
+
+fun fileTo2DList(fileName: String): List<List<Char>> {
+    val listOfStrings = fileToList<String>(fileName)
+    return listOfStrings
+        .map {
+            it.toCharArray().toList()
+        }
+}
+
+inline fun <reified T> fileToList(fileName: String): List<T> {
+    val inputStream = object {}.javaClass.getResourceAsStream("/$fileName")
+        ?: return emptyList()
+
+    return inputStream.reader().readLines()
+        .mapNotNull { it.parseTo() }
+}
+
+inline fun <reified T> String.parseTo(): T? =
+    when (T::class) {
+        Int::class -> this.toIntOrNull()
+        Double::class -> this.toDoubleOrNull()
+        String::class -> this
+        else -> throw IllegalStateException("Unsupported Generic Type")
+    } as T
+
